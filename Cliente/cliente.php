18ea22951428106446a498ee88d97392ff101073
RigobertoPO <rperez@cobach.edu.mx>
2021-10-11 19:56:17 -0500

    Version 1 vista en clases

---
+++ Cliente/cliente.php
@@ -0,0 +1,19 @@
+<?php
+    $cliente=new SoapClient(null,array(
+        'location'=> "http://localhost/ejemplosoap9k/Server/servidor.php",
+        'uri'     => "http://localhost/ejemplosoap9k/Server/servidor.php",
+        'trace'   =>1)
+    );
+    try{
+        $num1=$_POST['numero1'];
+        $num2=$_POST['numero2'];
+         echo $return = $cliente->__soapCall("saludar",["Luis"]).'<br>';
+         echo $return = $cliente->__soapCall("Bienvenida",["Maria"]).'<br>';
+         echo $return= $cliente->__soapCall("sumar",[$num1,$num2]).'<br>';
+         echo $return= $cliente->__soapCall("restar",[$num1,$num2]).'<br>';
+    }
+    catch(SOAPFault $e){
+        echo 'error';
+        echo $e->getMessage();
+    }
+?>
---
+++ Cliente/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>calcular</title>
+</head>
+<body>
+    <form action="cliente.php" method="post">
+        Numero 1: <input type="text" name="numero1">
+        Numero 2: <input type="text" name="numero2">
+        <input type="submit" value="calcular">
+    </form>
+</body>
+</html>
